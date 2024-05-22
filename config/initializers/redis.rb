14c32904373c20a77ab2aa739b75d184fa5c8653
cyrilmuchemi <cyrilmuchemi@gmail.com>
2024-05-23 02:16:54 +0300

    Comment the local redis instance

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,8 +1,8 @@
-require 'redis'
+# require 'redis'
 
-if Rails.env.production?
-  uri = URI.parse(ENV['REDIS_URL'])
-  Redis.current = Redis.new(host: uri.host, port: uri.port, password: uri.password)
-else
-  Redis.current = Redis.new
-end
+# if Rails.env.production?
+#   uri = URI.parse(ENV['REDIS_URL'])
+#   Redis.current = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+# else
+#   Redis.current = Redis.new
+# end
